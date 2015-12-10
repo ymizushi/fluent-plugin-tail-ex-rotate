8f5d8b0ccc4d509dc62c1beddc599fe7fa95e9bb
ymizushi <mizushi@gmail.com>
2015-12-10 23:54:29 +0900

    Correspond to add exclude_path option to support exclusion of files. See https://github.com/fluent/fluentd/pull/561

--- lib/fluent/plugin/in_tail_ex_rotate.rb
+++ lib/fluent/plugin/in_tail_ex_rotate.rb
@@ -8,16 +8,25 @@ module Fluent
     def expand_paths
       date = Time.now - @expand_rotate_time
       paths = []
+
+      excluded = @exclude_path.map { |path| path = date.strftime(path); path.include?('*') ? Dir.glob(path) : path }.flatten.uniq
       @paths.each { |path|
         path = date.strftime(path)
         if path.include?('*')
-          paths += Dir.glob(path)
+          paths += Dir.glob(path).select { |p|
+            if File.readable?(p)
+              true
+            else
+              log.warn "#{p} unreadable. It is excluded and would be examined next time."
+              false
+            end
+          }
         else
           # When file is not created yet, Dir.glob returns an empty array. So just add when path is static.
           paths << path
         end
       }
-      paths
+      paths - excluded
     end
   end
 end
